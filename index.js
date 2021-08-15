57ad4a4d33ec66b10058d0cc64c1e6ddabf65b58
Kat <bolivarkat@yahoo.com>
2021-08-15 11:06:57 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+function removeMain() {
+    document.getElementById('main').remove();
+}
+removeMain();
+
+const newHeader = document.createElement("h1");
+function addH1() {
+    newHeader.id = "victory";
+    newHeader.innerHTML = "<h1 id = 'victory'> Kat is the champion</h1>"
+}
+addH1();
